5e2b010c1e7699ce223367783d8a8639f26f41b8
sag <a.smln@yandex.ru>
2026-09-10 22:15:12 +0300

    init commit

---
+++ src/main/java/sag/example/spring_boot_anatom/SpringBootAnatomApplication.java
@@ -0,0 +1,13 @@
+package sag.example.spring_boot_anatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAnatomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAnatomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sag/example/spring_boot_anatom/SpringBootAnatomApplicationTests.java
@@ -0,0 +1,13 @@
+package sag.example.spring_boot_anatom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAnatomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
