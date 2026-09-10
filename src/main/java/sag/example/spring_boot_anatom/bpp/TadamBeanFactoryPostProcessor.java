f81544920584c1a29dfcd2bcfb07d94b93871ab0
sag <a.smln@yandex.ru>
2026-09-11 02:33:05 +0300

    Добавил BeanFactoryPostProcessor.

---
+++ src/main/java/sag/example/spring_boot_anatom/bpp/TadamBeanFactoryPostProcessor.java
@@ -0,0 +1,24 @@
+package sag.example.spring_boot_anatom.bpp;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TadamBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    public TadamBeanFactoryPostProcessor() {
+        IO.println("---\uD83C\uDFED[BFPP] Создание кастомного BeanFactoryPostProcessor.");
+        IO.println("   ✅[BFPP] Штука, которая работает с BeanDefinition (описаниями бинов).");
+        IO.println("   Такая же штуковина отвечает за то, чтобы превратить строчку @Value(\"${server.port}\") в реальный порт 8080.");
+    }
+
+    @Override
+    public void postProcessBeanFactory(@NonNull ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition beanDefinition = beanFactory.getBeanDefinition("beanForHisLifeCycleDissection");
+        beanDefinition.setDescription("Убийца - дворецкий!");
+    }
+}
